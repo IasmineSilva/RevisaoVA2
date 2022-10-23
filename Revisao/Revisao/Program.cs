43143b44723d5e7f6b67a51a0685ae46fece98e8
IasmineSilva <iasmine948@gmail.com>
2022-10-23 15:07:37 -0300

    teste revisao

---
+++ Revisao/Revisao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public class A
+    {
+        private int _value = 10;
+
+        public class B : A
+        {
+            public int GetValue()
+            {
+                return _value;
+            }
+        }
+    }
+
+    public class C : A
+    {
+           public int GetValue()
+          {
+               return _value;
+           }
+    }
+    public class AcessExample
+    {
+        public static void Main(string[] args)
+        
+        {
+            var b = new A.B();
+            Console.WriteLine(b.GetValue());
+        }
+    }
+}
